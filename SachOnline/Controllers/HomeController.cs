8ac77d67bc2c3bf34a7c03eb0591732b91d48aea
datnguyen13021302 <21280103e0017@student.tdmu.edu.vn>
2023-10-18 17:12:07 +0700

    bvnv

--- SachOnline/Controllers/HomeController.cs
+++ SachOnline/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SachOnline.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.  gvv";
 
             return View();
         }
